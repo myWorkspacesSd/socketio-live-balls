eb2ec16a44e1d820f73fc9bada0c558b18d7fb5a
myWorkspacesSd <myWorkspacesSd>
2022-09-07 11:15:33 +0300

    env changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getEnv', function(req, res, next) {
+  const envData = require('../config/env.json')[process.env.NODE_ENV || "development"];
+  res.json(envData);
+});
+
 module.exports = router;
